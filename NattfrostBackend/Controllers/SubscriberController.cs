d3ac081467a2b113fa1d4fcfaaae03be8a0f8040
Whezzan <d.skonneberg@gmail.com>
2026-05-22 11:51:00 +0200

    Add SubscriberController. Add DTO.

---
+++ NattfrostBackend/Controllers/SubscriberController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.EntityFrameworkCore;
+using NattfrostBackend.Data;
+
+
+namespace NattfrostBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SubscriberController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public SubscriberController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ NattfrostBackend/DTOs/CreateSubscribeRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace NattfrostBackend.DTOs
+{
+    public class CreateSubscribeRequest
+    {
+        public string Email { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+    }
+}
